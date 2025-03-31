7d8eaded389332a3fb1ba9fec4286f61276e5816
OJddJO <jeremyli2906@hotmail.com>
2025-03-31 17:36:53 +0200

    Hello world

--- bootloader.asm
+++ bootloader.asm
@@ -1,8 +1,30 @@
 bits    16
 org     0x7C00
 
-boot_main:
-    jmp boot_main
 
-times 510-($-$$) db 0
+main:
+    mov     si,     helloWorld
+    call    print
+    jmp     $
+
+helloWorld  db 'Hello World !', 0
+print:
+    ; Print a string
+    ; Args:
+    ;   si: pointer to the string to print
+    mov     ah,     0x0E
+    .loop:
+        mov     al,     byte [si]
+        cmp     al,     0
+        je      .end
+
+        int     0x10
+
+        inc     si
+        jmp     .loop
+
+    .end:
+        ret
+
+times 510 - ($-$$) db 0
 dw 0xAA55
