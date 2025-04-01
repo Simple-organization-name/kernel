aed67dee20102987b974c8e159613c53ea2f7d4c
OJddJO <jeremyli2906@hotmail.com>
2025-04-01 17:48:41 +0200

    Major formatting

--- bootloader.asm
+++ bootloader.asm
@@ -3,82 +3,92 @@
 
 mov     [bootDriveId],  dl      ; Keep this, might be handy later on
 
-realMain:
-    ; Init 16-bit mode temporary stack
-    mov     bp,     0x7BFF
-    mov     sp,     bp
+realMode:
+    .realMain:
+        ; Init 16-bit mode temporary stack
+        mov     bp,     0x7BFF
+        mov     sp,     bp
 
-    ; Enabling A20-gate
-    mov     ax,     0x2403      ; Ask if A20-gate is supported
-    int     0x15
-    jc      a20NotSupported
+        ; Enabling A20-gate
+        mov     ax,     0x2403      ; Ask if A20-gate is supported
+        int     0x15
+        jc      .a20NotSupported
 
-    mov     ax,     0x2402      ; Check the status of the A20-gate
-    int     0x15
-    jc      a20StatusError
-    cmp     al,     1
-    jmp     .a20Enabled
+        mov     ax,     0x2402      ; Check the status of the A20-gate
+        int     0x15
+        jc      .a20StatusError
+        cmp     al,     1
+        jmp     .a20Enabled
 
-    mov     ax,     0x2401      ; Activate the A20-gate
-    int     0x15
-    jc      a20NotToggleableByBIOS
+        mov     ax,     0x2401      ; Activate the A20-gate
+        int     0x15
+        jc      .a20NotToggleableByBIOS
 
-realMain.a20Enabled:
-    mov     si,     a20Enabled
-    call    _print16
+    .a20Enabled:
+        mov     si,     .a20EnabledMsg
+        call    .print
 
-    ; Switch to 32-bit "protected" mode
-    cli         ; disable interrupts
-    lgdt    [gdt32.descriptor]    ; load gdt
-    
-    jmp     $
-    ; switch to protected mode first
-    jmp     0x8:protectedMain
+        ; Switch to 32-bit "protected" mode
+        cli                             ; disable interrupts
+        lgdt    [gdt32.descriptor]      ; load gdt
 
-a20NotSupported:
-a20StatusError:
-a20NotToggleableByBIOS:
-    mov     si,     a20Error
-    call    _print16
-    jmp     $
+        ; Set up Protected Mode
+        mov     eax,    cr0
+        or      eax,    1
+        mov     cr0,    eax
 
-a20Enabled db "A20 enabled", 0x0A, 0x0D, 0
-a20Error db "Error when trying to enable A20", 0x0A , 0x0D, 0
+        jmp     0x8:protectedMode.protectedMain
+        hlt
 
-_print16:
-    ; Print a string in 16 bit mode
-    ; Args:
-    ;   si: pointer to the string to print
-    mov     ah,     0x0E
-    .loop:
-        mov     al,     byte [si]
-        cmp     al,     0
-        je      .end
+    .a20NotSupported:
+    .a20StatusError:
+    .a20NotToggleableByBIOS:
+        mov     si,     .a20ErrorMsg
+        call    .print
+        jmp     $
 
-        int     0x10
+    .a20EnabledMsg db "A20 enabled", 0x0A, 0x0D, 0
+    .a20ErrorMsg db "Error when trying to enable A20", 0x0A , 0x0D, 0
 
-        inc     si
-        jmp     .loop
-    .end:
-        ret
+    .print:
+        ; Print a string in 16 bit mode
+        ; Args:
+        ;   si: pointer to the string to print
+        mov     ah,     0x0E
+        .print.loop:
+            mov     al,     byte [si]
+            cmp     al,     0
+            je      .print.end
 
-%include "gdt32.inc"
+            int     0x10
 
+            inc     si
+            jmp     .print.loop
+        .print.end:
+            ret
 
-bits    32
-protectedMain:
-
-    jmp $
-    ; switch to long mode first
-    jmp     0x8:longMain
+    %include "gdt32.inc"
 
 
-bits    64
-longMain:
-    jmp $
+[bits    32]
+
+protectedMode:
+    .protectedMain:
+
+        jmp $
+
+        ; switch to long mode first
+        jmp     0x8:longMode.longMain
+
+
+[bits    64]
+
+longMode:
+    .longMain:
+        jmp $
 
 bootDriveId     db  0
-    
+
 
 times   510 - ($-$$) db 0   ; Fill up rest of first sector 
 dw      0xAA55              ; Boot signature to tell BIOS it can boot the disk
