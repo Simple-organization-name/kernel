c173b9e26016e4e1b0cdd428de227a07075f1318
OJddJO <jeremyli2906@hotmail.com>
2025-07-03 21:14:18 +0200

    bump

--- makefile
+++ makefile
@@ -1,10 +1,20 @@
+all: iso emul
+
+iso:
+	nasm -f win64 src/main.asm -o build/main.o
+	lld-link /subsystem:efi_application /nodefaultlib /entry:efi_main build/main.o /out:iso/EFI/BOOT/BOOTx64.EFI
+	cp -f iso/EFI/BOOT/BOOTx64.EFI drive/BOOTx64.EFI
+# xorriso -as mkisofs -R -J -joliet-long -o SOS.ISO -V "UEFI_BOOT" -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot -isohybrid-gpt-basdat iso/
+	xorriso -as mkisofs -iso-level 3 -o SOS.ISO -full-iso9660-filenames -volid "SOS" -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot -isohybrid-gpt-basdat iso/
+# genisoimage -A "SOS" -V "SOS" -volset "SOS" -r -v -o SOS.ISO -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot iso/
+
+emul:
+	qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -cdrom SOS.ISO -m 512M -net none
+
+shell:
+	qemu-system-x86_64 -bios OVMF.fd -net none -drive format=raw,file=fat:rw:drive/
+
 setup:
 	sudo apt install lld genisoimage qemu-system ovmf
 
-all:
-	nasm -f win64 src/main.asm -o build/main.o
-	lld-link -subsystem:efi_application -nodefaultlib -entry:EfiMain build/main.o -out:iso/EFI/BOOT/BOOTx64.EFI
-	cp -f iso/EFI/BOOT/BOOTx64.EFI drive/BOOTx64.EFI
-	genisoimage -U -A "UEFI-ISO" -V "UEFI-ISO" -volset "UEFI-ISO" -J -joliet-long -r -v -T -o output.iso -efi-boot EFI/BOOT/BOOTx64.EFI -no-emul-boot iso/
-	qemu-system-x86_64 -bios OVMF.fd -net none -drive format=raw,file=fat:rw:drive/
-# qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -cdrom output.iso -net none
+.PHONY: all iso emul setup
--- src/main.asm
+++ src/main.asm
@@ -1,24 +1,65 @@
 [bits 64]
-%include "src/efi_struct.inc"
+
+section .text
+global efi_main
+struc EFI_TABLE_HEADER
+    .Signature              resq 1
+    .Revision               resd 1
+    .HeaderSize             resd 1
+    .CRC32                  resd 1
+    .Reserved               resd 1
+endstruc
+
+struc EFI_SYSTEM_TABLE
+    .Hdr                    resb EFI_TABLE_HEADER_size 
+    .FirmwareVendor         resq 1
+    .FirmwareRevision       resd 1
+    .offset1                resd 1 ; offset because need to be aligned
+    .ConsoleInHandle        resq 1
+    .ConIn                  resq 1
+    .ConsoleOutHandle       resq 1
+    .ConOut                 resq 1 ; pointer to the _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+    .StandardErrorHandle    resq 1
+    .StdErr                 resq 1
+    .RuntimeServices        resq 1
+    .BootServices           resq 1
+    .NumberOfTableEntries   resq 1
+    .ConfigurationTable     resq 1
+endstruc
+
+struc EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+    .Reset                  resq 1
+    ; Args:
+    ;   rcx (qword): The EFI_SYSTEM_TABLE pointer
+    ;   rdx (qword): The string to print
+    .OutputString           resq 1
+    .TestString	            resq 1
+    .QueryMode	            resq 1
+    .SetMode	            resq 1
+    .SetAttribute           resq 1
+    .ClearScreen            resq 1
+    .SetCursorPosition      resq 1
+    .EnableCursor           resq 1
+    .Mode                   resq 1
+endstruc
+
+efi_main:
+    ; UEFI boots with the pointer to the efi system table in rdx
+    mov     [ptr_efiSystemTable],   rdx
+
+    ; Setup stack for the EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString()
+    sub     rsp,    8 ; Don't know why
+    mov     rcx,    [rdx + EFI_SYSTEM_TABLE.ConOut]
+    mov     rdx,    hello
+    call    [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
+    add     rsp,    8
+
+    jmp     $
+    ; xor     rax,    rax
+    ; ret
 
 section .bss
     ptr_efiSystemTable  resq 1
 
 section .data
-    hello dw __utf16__ "Hello world !", 0xd, 0xa, 0
-
-section .text
-    global EfiMain
-    EfiMain:
-        ; UEFI boots with the pointer to the efi system table in rdx
-        mov     [ptr_efiSystemTable],   rdx
-
-        ; Setup stack for the _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString()
-        sub     rsp,    4*8 ; Don't know why
-        mov     rcx,    [rdx + EFI_SYSTEM_TABLE.ConOut]
-        mov     rdx,    hello
-        call    [rcx + _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
-        add     rsp,    4*8
-
-        xor     rax,    rax
-        ret
+    hello dw __utf16__('Hello world !'), 0x000D, 0x000A, 0x0000
