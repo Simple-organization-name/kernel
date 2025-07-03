ef51d17bf17e7be7e4da50922aac97fff7ef1bb6
OJddJO <jeremyli2906@hotmail.com>
2025-07-03 22:29:38 +0200

    Bump

--- makefile
+++ makefile
@@ -1,20 +1,23 @@
-all: iso emul
+BOOT_NAME = BOOTX64.EFI
+ISO_ENTRY = EFI/BOOT/$(BOOT_NAME)
+
+OVMF_PATH = /usr/share/ovmf/OVMF.fd
+
+all: iso shell
 
 iso:
 	nasm -f win64 src/main.asm -o build/main.o
-	lld-link /subsystem:efi_application /nodefaultlib /entry:efi_main build/main.o /out:iso/EFI/BOOT/BOOTx64.EFI
-	cp -f iso/EFI/BOOT/BOOTx64.EFI drive/BOOTx64.EFI
-# xorriso -as mkisofs -R -J -joliet-long -o SOS.ISO -V "UEFI_BOOT" -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot -isohybrid-gpt-basdat iso/
-	xorriso -as mkisofs -iso-level 3 -o SOS.ISO -full-iso9660-filenames -volid "SOS" -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot -isohybrid-gpt-basdat iso/
-# genisoimage -A "SOS" -V "SOS" -volset "SOS" -r -v -o SOS.ISO -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot iso/
+	lld-link /subsystem:efi_application /nodefaultlib /entry:efi_main build/main.o /out:iso/$(ISO_ENTRY)
+	cp -f iso/$(ISO_ENTRY) drive/$(BOOT_NAME)
+	xorriso -as mkisofs -iso-level 3 -o SOS.ISO -full-iso9660-filenames -volid "SOS" -eltorito-alt-boot -e $(ISO_ENTRY) -no-emul-boot -isohybrid-gpt-basdat iso/
 
 emul:
-	qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -cdrom SOS.ISO -m 512M -net none
+	qemu-system-x86_64 -bios $(OVMF_PATH) -cdrom SOS.ISO -m 512M -boot d -net none
 
 shell:
-	qemu-system-x86_64 -bios OVMF.fd -net none -drive format=raw,file=fat:rw:drive/
+	qemu-system-x86_64 -bios $(OVMF_PATH) -net none -drive format=raw,file=fat:rw:drive/
 
 setup:
 	sudo apt install lld genisoimage qemu-system ovmf
 
-.PHONY: all iso emul setup
+.PHONY: all iso emul shell setup
--- src/main.asm
+++ src/main.asm
@@ -1,62 +1,23 @@
 [bits 64]
 
 section .text
-global efi_main
-struc EFI_TABLE_HEADER
-    .Signature              resq 1
-    .Revision               resd 1
-    .HeaderSize             resd 1
-    .CRC32                  resd 1
-    .Reserved               resd 1
-endstruc
+    %include "src/efi.inc"
 
-struc EFI_SYSTEM_TABLE
-    .Hdr                    resb EFI_TABLE_HEADER_size 
-    .FirmwareVendor         resq 1
-    .FirmwareRevision       resd 1
-    .offset1                resd 1 ; offset because need to be aligned
-    .ConsoleInHandle        resq 1
-    .ConIn                  resq 1
-    .ConsoleOutHandle       resq 1
-    .ConOut                 resq 1 ; pointer to the _EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
-    .StandardErrorHandle    resq 1
-    .StdErr                 resq 1
-    .RuntimeServices        resq 1
-    .BootServices           resq 1
-    .NumberOfTableEntries   resq 1
-    .ConfigurationTable     resq 1
-endstruc
+    global efi_main
+    efi_main:
+        ; UEFI boots with the pointer to the efi system table in rdx
+        mov     [ptr_efiSystemTable],   rdx
 
-struc EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
-    .Reset                  resq 1
-    ; Args:
-    ;   rcx (qword): The EFI_SYSTEM_TABLE pointer
-    ;   rdx (qword): The string to print
-    .OutputString           resq 1
-    .TestString	            resq 1
-    .QueryMode	            resq 1
-    .SetMode	            resq 1
-    .SetAttribute           resq 1
-    .ClearScreen            resq 1
-    .SetCursorPosition      resq 1
-    .EnableCursor           resq 1
-    .Mode                   resq 1
-endstruc
+        ; Setup stack for the EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString()
+        sub     rsp,    8 ; Align stack to 16 bytes
+        mov     rcx,    [rdx + EFI_SYSTEM_TABLE.ConOut]
+        mov     rdx,    hello
+        call    [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
+        add     rsp,    8
 
-efi_main:
-    ; UEFI boots with the pointer to the efi system table in rdx
-    mov     [ptr_efiSystemTable],   rdx
-
-    ; Setup stack for the EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString()
-    sub     rsp,    8 ; Don't know why
-    mov     rcx,    [rdx + EFI_SYSTEM_TABLE.ConOut]
-    mov     rdx,    hello
-    call    [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
-    add     rsp,    8
-
-    jmp     $
-    ; xor     rax,    rax
-    ; ret
+        jmp     $
+        ; xor     rax,    rax
+        ; ret
 
 section .bss
     ptr_efiSystemTable  resq 1
