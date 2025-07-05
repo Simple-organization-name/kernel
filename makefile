581f262c9acf1970075bb29b913636e8ae01525c
OJddJO <jeremyli2906@hotmail.com>
2025-07-05 14:08:54 +0200

    Bump

--- makefile
+++ makefile
@@ -6,12 +6,16 @@ OVMF_PATH = /usr/share/ovmf/OVMF.fd
 all: iso emul
 
 iso:
-	nasm -f win64 src/main.asm -o build/main.o
-	lld-link /subsystem:efi_application /nodefaultlib /entry:efi_main build/main.o /out:iso/$(ISO_ENTRY)
+	nasm -f win64 src/main.asm -I src/ -o build/main.o -Werror -Wall
+	lld-link /subsystem:efi_application /nodefaultlib /entry:efiMain build/main.o /out:iso/$(ISO_ENTRY)
+	cp -f iso/$(ISO_ENTRY) drive/$(BOOT_NAME)
 	xorriso -as mkisofs -iso-level 3 -o SOS.ISO -full-iso9660-filenames -volid "SOS" -eltorito-alt-boot -e $(ISO_ENTRY) -no-emul-boot -isohybrid-gpt-basdat iso/
 
 emul:
 	qemu-system-x86_64 -drive format=raw,file=fat:rw:iso/ -bios $(OVMF_PATH) -net none
+
+shell:
+	qemu-system-x86_64 -drive format=raw,file=fat:rw:drive/ -bios $(OVMF_PATH) -net none
 
 setup:
 	sudo apt install lld genisoimage qemu-system ovmf
--- src/main.asm
+++ src/main.asm
@@ -1,26 +1,119 @@
 [bits 64]
 
+section .bss
+    efiImageHandle              resq 1  ; Handle to the EFI image
+    efiSystemTable              resq 1  ; Pointer to the EFI system table
+
+    efiLoadedImageProtocol      resq 1  ; Pointer to the EFI loaded image protocol
+    efiSimpleFileSystemProtocol resq 1  ; Pointer to the EFI simple file system protocol
+    efiFileProtocol             resq 1  ; Pointer to the EFI file protocol
+
+section .data
+    hello dw __utf16__("Hello world from SOS !"), 0xd, 0xa, 0
+
+    ok dw __utf16__("OK"), 0xd, 0xa, 0
+
+    creatingLogFile dw __utf16__("Creating log file..."), 0xd, 0xa, 0
+    getEfiLoadedImageProtocolError dw __utf16__("Error getting the EFI_LOADED_IMAGE_PROTOCOL"), 0xd, 0xa, 0
+    getEfiLoadedImageProtocolSuccess dw __utf16__("EFI_LOADED_IMAGE_PROTOCOL successfully retrieved"), 0xd, 0xa, 0
+    getEfiSimpleFileSystemProtocolError dw __utf16__("Error getting the EFI_SIMPLE_FILE_SYSTEM_PROTOCOL"), 0xd, 0xa, 0
+    getEfiSimpleFileSystemProtocolSuccess dw __utf16__("EFI_SIMPLE_FILE_SYSTEM_PROTOCOL successfully retrieved"), 0xd, 0xa, 0
+    getEfiFileProtocolError dw __utf16__("Error opening the root directory"), 0xd, 0xa, 0
+    getEfiFileProtocolSuccess dw __utf16__("EFI_FILE_PROTOCOL successfully retrieved"), 0xd, 0xa, 0
+
 section .text
-    %include "src/efi.inc"
+    align 8
+    %include "efi.inc"
 
-    global efi_main
-    efi_main:
-        ; UEFI boots with the pointer to the efi system table in rdx
-        mov     [ptr_efiSystemTable],   rdx
+    global efiMain
+    efiMain:
+        ; UEFI boots with the pointer to the efi image handle in rcx and efi system table in rdx
+        mov     [efiImageHandle],   rcx
+        mov     [efiSystemTable],   rdx
 
-        ; Setup stack for the EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString()
-        sub     rsp,    8 ; Align stack to 16 bytes
-        mov     rcx,    [rdx + EFI_SYSTEM_TABLE.ConOut]
         mov     rdx,    hello
-        call    [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
-        add     rsp,    8
+        call    print
+
+        mov     rdx,    creatingLogFile
+        call    print
+        call    createLogFile
 
         jmp     $
         ; xor     rax,    rax
         ; ret
 
-section .bss
-    ptr_efiSystemTable  resq 1
+    print:
+        ; rdx: Pointer to the string to print (must be UTF-16)
+        mov     rax,    [efiSystemTable]
+        mov     rcx,    [rax + EFI_SYSTEM_TABLE.ConOut]
+        call    [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
+        ret
 
-section .data
-    hello dw __utf16__('Hello world !'), 0x000D, 0x000A, 0x0000
+    createLogFile:
+        ; Create a log file in the EFI file system
+
+        ; Get the EFI_LOADED_IMAGE_PROTOCOL
+        mov     rcx,    [efiImageHandle]
+        mov     rdx,    EFI_LOADED_IMAGE_PROTOCOL_GUID
+        mov     r8,     efiLoadedImageProtocol
+        mov     rax,    [efiSystemTable]
+        mov     rax,    [rax + EFI_SYSTEM_TABLE.BootServices]
+        call    [rax + EFI_BOOT_SERVICES.HandleProtocol]
+
+        mov     rdx,    ok
+        call    print
+
+        cmp     rax,    0 ; Check if the protocol was successfully retrieved
+        je      .gotEfiLoadedImageProtocol ; if it is zero, jmp to the next section
+
+        ; Handle the error
+        mov     rdx,    getEfiLoadedImageProtocolError
+        call    print
+        jmp     $
+
+        .gotEfiLoadedImageProtocol:
+        mov     rdx,    getEfiLoadedImageProtocolSuccess
+        call    print
+
+        ; Get the SIMPLE_FILE_SYSTEM_PROTOCOL
+        mov     rcx,    [efiLoadedImageProtocol]
+        mov     rcx,    [rcx + EFI_LOADED_IMAGE_PROTOCOL.DeviceHandle] ; Get the device handle
+        mov     rdx,    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID
+        mov     r8,     efiSimpleFileSystemProtocol
+        mov     rax,    [efiSystemTable]
+        mov     rax,    [rax + EFI_SYSTEM_TABLE.BootServices]
+        call    [rax + EFI_BOOT_SERVICES.HandleProtocol]
+
+        cmp     rax,    0 ; Check if the protocol was successfully retrieved
+        je      .gotEfiSimpleFileSystemProtocol ; if it is zero, jmp to the next section
+
+        ; Handle the error
+        mov     rdx,    getEfiSimpleFileSystemProtocolError
+        call    print
+        jmp     $
+
+        .gotEfiSimpleFileSystemProtocol:
+        mov     rdx,    getEfiSimpleFileSystemProtocolSuccess
+        call    print
+
+        ; Open the root directory of the file system
+        mov     rcx,    [efiSimpleFileSystemProtocol]
+        mov     rdx,    efiFileProtocol
+        mov     rax,    [efiSystemTable]
+        call    [rax + EFI_SIMPLE_FILE_SYSTEM_PROTOCOL.OpenVolume]
+
+        cmp     rax,    0 ; Check if the root directory was successfully opened
+        je      .gotEfiFileProtocol ; if it is zero, jmp to the next section
+
+        ; Handle the error
+        mov     rdx,    getEfiFileProtocolError
+        call    print
+        jmp     $
+
+        .gotEfiFileProtocol:
+        mov     rdx,    getEfiFileProtocolSuccess
+        call    print
+
+        ; Create a new file named "log.txt"
+
+        jmp     $
