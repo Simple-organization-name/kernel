3afd2922cc06f6baed63e1ff6802750c0e692452
DiggerDwarf <135308092+DiggerDwarf@users.noreply.github.com>
2025-12-07 23:10:31 +0100

    more UHCI docs

--- include/UHCI.h
+++ include/UHCI.h
@@ -33,40 +33,40 @@ typedef struct _uhci_td {
         struct {
             uint8_t __r1: 2,
                     shortPacketDetect: 1,
-                    errorLimit: 2,
-                    lowSpeedDevice: 1,
-                    isochronous: 1,
-                    interruptOnFrameEnd: 1;
+                    errorLimit: 2,  // 0 for no error limit, or max 3 for max error count allowed. see docs for accounted errors.
+                    lowSpeedDevice: 1,  // target device is low speed (1.5Mb/s). see token for device
+                    isochronous: 1, // welp like read the thing (btw isochronous are marked inactive no matter the errors)
+                    interruptOnFrameEnd: 1; // wether to issue interrupt when frame TD is in is completed
             union {
                 uint8_t whole;
                 struct {
-                    uint8_t active: 1,
-                            stalled: 1,
-                            dataBufferError: 1,
-                            babbleDetected: 1,
-                            NAK_Received: 1,
-                            CRCerr_or_timeOut: 1,
-                            bitstuffError: 1,
+                    uint8_t active: 1,  // set to 1 to execute, written 0 on completion or stall
+                            stalled: 1, // stall happens when error or device just says welp yes but frick you
+                            dataBufferError: 1, // something like speeds not matching (overrun/underrun) -> cause of actLen != maxLen
+                            babbleDetected: 1,  // refer to bandwidth reclamation packets
+                            NAK_Received: 1,    // negative acknowledgement => should mean "not ready" ?
+                            CRCerr_or_timeOut: 1,   // pretty staightforward
+                            bitstuffError: 1,   // data stream contained more than 6 0b1 in a row, refer to further protocol docs ig
                             __r: 1;
                 };
             } status;
             uint16_t __r2: 5,
-                    actLen: 11;
+                    actLen: 11; // must be set to 0 at start, written to on complete
         };
     } controlAndStatus;
     union {
         uint32_t whole;
         struct {
-            uint32_t maxLen: 11,
+            uint32_t maxLen: 11,    // put n-1 in here, max 0x4FF = 1280 bytes, magic value 0x7FF for Null Data packet
                     __r: 1,
                     dataToggle: 1,
-                    endPoint: 4,
-                    deviceAddress: 7,
-                    packetID: 8;
+                    endPoint: 4,    // device endpoint selector
+                    deviceAddress: 7,   // device selector
+                    packetID: 8;    // IN = 0x69, OUT = 0xE1, SETUP = 0x2D, other values are invalid
         };
     } token;
-    uint32_t bufferPointer;
-    uint32_t forYou[4];
+    uint32_t bufferPointer; // physical address of transaction data buffer, of size maxLen and arbitrary alignment
+    uint32_t forYou[4]; // ig do what we want here, maybe put info for our own traversal ? or discard entirely since TD's are 16b-aligned
 } UHCI_TransferDescriptor;
 
 typedef struct _uhci_qh {
