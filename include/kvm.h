4f96b6be1567aab55ca3d04147a6f3db7c8b15b9
OJddJO <95376647+OJddJO@users.noreply.github.com>
2026-02-04 19:01:27 +0100

    Bump

--- include/kvm.h
+++ include/kvm.h
@@ -4,12 +4,17 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define VM_START (1<<21)
+#define VM_START_ARR    ((uint16_t[4]){2, 0, 0, 0})
+#define VM_START        (0x10000000000UL)
+
+#define VM_START 
 
 typedef struct _VirtMemory {
     void                *addr;
     size_t              size;
     struct _VirtMemory  *next;
 } VirtMemory;
+
+size_t kvmalloc(size_t size);
 
 #endif
--- src/kernel/kvm.c
+++ src/kernel/kvm.c
@@ -3,6 +3,21 @@
 #include "buddy.h"
 #include "memmap.h"
 
-void vmalloc(size_t size) {
-    (void)size;
+/**
+ * Allocate the smallest block fitting size
+ * And map it in an empty slot
+ * \return The size of the block allocated
+ */
+size_t kvmalloc(size_t size) {
+    size_t totalAllocated = 0;
+    for (int8_t i = BUDDY_MAX_ORDER; i >= 0; i--) {
+        while (size > BUDDY_IDX_BLOCK_SIZE(i)) {
+            uint16_t idx[4] = {0};
+            if (!findEmptySlotPageIdx(PTE_PT, idx))
+                return 0;
+
+            PhysAddr addr = buddyAlloc(i);
+            
+        }
+    }
 }
